cccf0eaa32bc62b445f576ab5ab81e77d66d0ee1
joaogdiass <joaogdemello@outlook.com>
2023-04-05 12:12:16 -0300

    Exercicio 3

--- script.js
+++ script.js
@@ -8,3 +8,22 @@ let num2= 2 ;
 let res= num1%num2 ;
 console.log(res);
 
+//CALCULANDO IMC DE UMA PESSOA
+let peso =prompt("Digite seu peso")
+let altura=prompt("Digite sua altura em metros")
+
+imc = peso/(altura*altura)
+
+document.write("Seu imc é ",imc.toFixed(1))
+
+document.write("<br></br>")
+
+if(imc<18.5){
+    document.write("Seu IMC está abaixo do peso ideal")
+}else if(imc>=18.5 && imc<24.9){
+    document.write("Seu IMC está ideal")
+}else if(imc>=24.9){
+document.write("Seu IMC está acima do ideal")
+}
+
+document.write("<br></br>")
