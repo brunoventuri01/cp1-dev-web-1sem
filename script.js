f017a2252ca011d1e29cf95c6073e92ee6e47b4a
brunoventuri01 <brunomitopvp@gmail.com>
2023-04-05 12:08:15 -0300

    Exercicio 2

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 //CRIANDO UMA VARIÁVEL INDEFINIDA
 let varindefinida  
 console.log(varindefinida);
+
+//USANDO OPERADOR % EM JAVASCRIPT COM SAIDA DEMONSTRADA
+let num1= 7 ;
+let num2= 2 ;
+let res= num1%num2 ;
+console.log(res);
+
